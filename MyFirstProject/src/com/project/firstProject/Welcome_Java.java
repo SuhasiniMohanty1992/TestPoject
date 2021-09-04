51ab13fbafb147c845c04f13b7d4e79a828aeaee
SatyaMallick1991 <90056984+SatyaMallick1991@users.noreply.github.com>
2021-09-04 23:33:02 +0530

    Added Addition Functionality

--- MyFirstProject/src/com/project/firstProject/Welcome_Java.java
+++ MyFirstProject/src/com/project/firstProject/Welcome_Java.java
@@ -5,5 +5,12 @@ public class Welcome_Java {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome To Python");
+		
+		int a=10;
+		int b=10;
+		
+		 int Sum=a +b;
+		 
+		 System.out.println(Sum);
 	}
 }
