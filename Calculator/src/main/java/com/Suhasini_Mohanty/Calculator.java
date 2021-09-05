4636026c028a71d56c380dcde6b76579cd7eee78
SatyaMallick1991 <90056984+SatyaMallick1991@users.noreply.github.com>
2021-09-05 10:40:12 +0530

    Log4J Update

--- Calculator/src/main/java/com/Suhasini_Mohanty/Calculator.java
+++ Calculator/src/main/java/com/Suhasini_Mohanty/Calculator.java
@@ -1,7 +1,11 @@
 package com.Suhasini_Mohanty;
 
+import org.apache.log4j.Logger;
+
 public class Calculator {
 
+	static Logger log = Logger.getLogger(Calculator.class.getName());
+	
 	public int addTwoNumbers(int a, int b)
 	{
 		int sum=0;
@@ -10,9 +14,11 @@ public class Calculator {
 	}
 	
 	public static void main(String[] args) {
-		
+	    log.debug("Project Started Executing....");
+	      
 		Calculator cal=new Calculator();
 		int result=cal.addTwoNumbers(5, 6);
+		log.info("Result" + result);
 		System.out.println(result);
 	}
 }
